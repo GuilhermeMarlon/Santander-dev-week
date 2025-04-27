6741c0fef684857a61808a88008b1d5a3e4e18f3
GuilhermeMarlon <guilhermemarlon.contato@gmail.com>
2025-04-27 16:04:42 -0300

    primeiro commit

---
+++ src/test/java/DIO/me/Santander/SantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package DIO.me.Santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
